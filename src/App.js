7c8d13c304ae253ca2d863d19264fe1ffb7ee4e1
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-09 19:36:01 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+Here is your updated app.js with a new button added. For demonstration, I've added the button inside the main <div className='App'>, below the imported components. You can edit its position or functionality as needed.
+
+```javascript
 /* eslint-disable */
 import React, { Component } from 'react'
 import './App.css';
@@ -12,17 +15,28 @@ import resumeData from './components/resumeData'
 
 class App extends Component {
   render() {
-  return (
-  <div className='App'>
-  <Header resumeData={resumeData}/>
-  <About resumeData={resumeData}/>
-  <Resume resumeData={resumeData}/>
-  <Portfolio resumeData={resumeData}/>
-  {/* <Testimonials resumeData={resumeData}/> */}
-  <ContactUs resumeData={resumeData}/>
-  <Footer resumeData={resumeData}/>
-  </div>
-  );
+    return (
+      <div className='App'>
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        {/* <Testimonials resumeData={resumeData}/> */}
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+
+        {/* New Button Added */}
+        <button
+          onClick={() => alert('New button clicked!')}
+          style={{marginTop: '20px'}}
+        >
+          New Button
+        </button>
+      </div>
+    );
   }
-  }
-  export default App;
+}
+export default App;
+```
+
+Let me know if you want the button to do something specific or be placed elsewhere!
