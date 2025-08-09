bf9d0bf17a982e6245ae58eea9b1715cb4ce9473
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-09 19:42:41 +0530

    test

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,27 +1,18 @@
 ```javascript
-/* eslint-disable */
-import React, { Component } from 'react'
+// src/components/ContactUs.js
 
-export default class ContactUs extends Component {
-render() {
-let resumeData = this.props.resumeData;
-return (
-<section id='contact'>
-<div className='row section-head'>
-<div className='ten columns'>
-<p className='lead'>
-Feel free to contact me on +91 9209579453 or sanchitsav@gmail.com. 
-</p>
-</div>
-</div>
-<div className='row'>
-<aside className='eighth columns footer-widgets'>
-<div className='widget'>
-</div>
-</aside>
-</div>
-</section>
-);
-}
-}
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>
+        You can reach us at: <strong>1234567</strong>
+      </p>
+    </div>
+  );
+};
+
+export default ContactUs;
 ```
