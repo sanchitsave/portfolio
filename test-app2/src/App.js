fb825f43b6704e9b5ec9d9639428812b0b22fc4d
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-09 22:34:09 +0530

    Add src/App.js with functional component

---
+++ test-app2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      Welcome to My React App
+    </div>
+  );
+}
+
+export default App;
