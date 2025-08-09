1cd3448624f23ae906d88c667897dcb57bd283af
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-09 23:22:35 +0530

    Create src/App.js with functional component and food menu

---
+++ test-app8/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import menuData from './data/menuData';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to My React App</h1>
+      <h2>Food Menu</h2>
+      <ul className="menu-list">
+        {menuData.map((item, idx) => (
+          <li key={idx} className="menu-item">
+            <span className="food-name">{item.name}</span> - <span className="food-price">${item.price.toFixed(2)}</span>
+            <div className="food-desc">{item.description}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
